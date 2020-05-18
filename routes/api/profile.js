96b38fd6443627cf666fe0edea95f899b6e2e324
Alok Tripathi <gnsalok@gmail.com>
2020-05-19 03:25:54 +0530

    added GET route to find user by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -98,8 +98,8 @@ router.post(
   }
 );
 
-// @route    GET api/profile/user/:user_id
-// @desc     Get profile by user ID
+// @route    GET api/profile
+// @desc     Get all profiles
 // @access   Public
 
 router.get('/', async (req, res) => {
@@ -112,4 +112,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get(
+  '/user/:user_id',
+  checkObjectId('user_id'),
+  async ({ params: { user_id } }, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: user_id,
+      }).populate('user', ['name', 'avatar']);
+
+      if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: 'Server error' });
+    }
+  }
+);
 module.exports = router;

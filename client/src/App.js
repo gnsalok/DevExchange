592152fee8c66263c6bee8aaf2f3612e548878ef
Alok Tripathi <gnsalok@gmail.com>
2020-08-27 15:46:05 +0530

    added reducx in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,25 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
